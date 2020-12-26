912be6cde9797bb4166a8eba0c56660e1bede83f
thatscold <usmaanfatal@gmail.com>
2020-12-26 10:56:00 +0500

    Palindrome Checker Made by me

---
+++ Pallindrome checker/app.js
@@ -0,0 +1,30 @@
+const text = document.getElementById('text');
+const resultContainer = document.getElementById('result-container');
+const result = document.getElementById('result');
+const checkBtn = document.getElementById('check');
+
+let answer = false;
+
+function evaluate() {
+   const revText = text.value
+      .split('')
+      .reverse()
+      .join('')
+      .toLowerCase()
+      .replaceAll(/\W/g, '');
+   console.log(revText);
+
+   if (revText === text.value.toLowerCase().replaceAll(/\W/g, '')) {
+      result.innerHTML = `Text is a Palindrome`;
+   } else {
+      result.innerHTML = `Text is not a Palindrome`;
+   }
+
+   resultContainer.classList.add('show');
+   setTimeout(() => {
+      resultContainer.classList.remove('show');
+   }, 4000);
+}
+
+//Event Listeners
+checkBtn.addEventListener('click', evaluate);
