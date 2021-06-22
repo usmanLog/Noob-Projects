c7d434c52b34722f89f04857b909ca64ae3479ee
thatscold <usmaanfatal@gmail.com>
2021-06-22 11:34:23 +0500

    redux-store

---
+++ redux-store/src/app/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Inventory } from '../features/inventory/Inventory.js';
+import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter.js';
+// Import the Cart component here.
+import { Cart } from '../features/cart/Cart.js';
+// Render the Cart component below <Inventory />
+export const App = (props) => {
+
+    const { state, dispatch } = props;
+
+    return (
+        <div>
+            <CurrencyFilter
+                currencyFilter={state.currencyFilter}
+                dispatch={dispatch}
+            />
+
+            <Inventory
+                inventory={state.inventory}
+                currencyFilter={state.currencyFilter}
+                dispatch={dispatch}
+            />
+
+            <Cart
+                cart={state.cart}
+                dispatch={dispatch}
+                currencyFilter={state.currencyFilter}
+            />
+        </div>
+    );
+};
---
+++ redux-store/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+import { App } from './app/App.js';
+// Import the store here.
+import { store } from './app/store.js'
+// Pass state and dispatch props to the <App /> component.
+const render = () => {
+  ReactDOM.render(
+    <App
+      dispatch={store.dispatch}
+      state={store.getState()}
+    />,
+    document.getElementById('root')
+  )
+};
+render();
+
+// Subscribe render to the store.
+store.subscribe(render);
+
